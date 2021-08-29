b509581b4df5a022004f8cd2c6195fb3e71bc2ba
Edward Ho <edwardhkmpp@hotmail.com>
2021-08-30 01:34:23 +1000

    6th commit

--- 8-1.py
+++ 8-1.py
@@ -12,3 +12,22 @@ def favorite_book(title):
 
 
 favorite_book("Alice in Wonderland")
+
+# 8-3, 8-4
+def make_shirt(msg_to_print='I love Python', size='large'):
+    print(f"The t-shirt size is {size}, message to print is {msg_to_print}")
+
+
+make_shirt(1, "LALALA")
+make_shirt(size=2, msg_to_print="kkk")
+make_shirt(size='medium')
+make_shirt()
+make_shirt(size='small', msg_to_print='uuu')
+
+# 8-5
+def describe_city(name, country='Australia'):
+    print(f"{name} is in {country}")
+
+describe_city('Sydney')
+describe_city('HK', 'HK')
+describe_city('Tokyo', 'Japan')
--- pets.py
+++ pets.py
@@ -3,3 +3,21 @@ print(pets)
 while 'cat' in pets:
     pets.remove('cat')
 print(pets)
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+def describe_pet2(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet2(pet_name='harry')
+describe_pet2('Sanna')
