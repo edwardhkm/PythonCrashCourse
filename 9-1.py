48c479082adf380390f3531a4fcdac7bdfc7c3b5
edwardhkm <edwardhkmpp@hotmail.com>
2021-08-31 16:58:44 +1000

    6th commit

--- 9-1.py
+++ 9-1.py
@@ -23,3 +23,25 @@ my_rest3 = Restaurant("New LULU", "Japanese")
 my_rest.describe_restaurant()
 my_rest2.describe_restaurant()
 my_rest3.describe_restaurant()
+
+# 9-3
+class User:
+    def __init__(self, first_name, last_name, gender, username):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.username = username
+
+    def describe_user(self):
+        print(f"{self.username}, {self.first_name}, {self.last_name}")
+
+    def greet_user(self):
+        print(f"Hello, {self.username}!")
+
+ed = User('ed', 'ho', 'M', 'HOED')
+zoe = User('zoe', 'zhou', 'F', 'ZhoZ')
+
+ed.greet_user()
+zoe.greet_user()
+ed.describe_user()
+zoe.describe_user()
