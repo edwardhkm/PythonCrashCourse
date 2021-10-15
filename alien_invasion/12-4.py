35a69e7d136959392e6500d8f739d9aee618207d
Edward Ho <edward.ho@nesa.nsw.edu.au>
2021-10-15 16:58:31 +1100

    update 12-4

--- alien_invasion/12-4.py
+++ alien_invasion/12-4.py
@@ -1,10 +1,31 @@
 import pygame
 
-class Rocket:
+class Rocket_game:
     def __init__(self):
         pygame.init()
+        self.screen = pygame.display.set_mode((1024, 768))
+        pygame.display.set_caption("Rocket moving")
+        self.bg_color = (230, 230, 230)
+        self.rocket = Rocket()
+
+    def run_game(self):
+
+    def _check_event(self):
+
+    def _check_keyup_events(self):
+
+    def _check_keydown_events(self):
+
+class Rocket:
+    def __init__(self, r_game):
+        self.rocket_game = r_game.screen
+        self.screen_rect = r_game.screen.get_rect()
+        self.moving_right = False
+        self.moving_left = False
+        self.image = pygame.image.load('images/ship.bmp')
+
 
 
 if __name__ == "__main__":
-    rocket = Rocket()
-    rocket.run_game()
+    rg = Rocket_game()
+    rg.run_game()
